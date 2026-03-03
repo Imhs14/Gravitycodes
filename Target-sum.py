8da47874d2fcb9e61f165177e3888bb55c76edff
imhs14 <heerashanker0214@gmail.com>
2026-03-04 00:23:26 +0530

    target-sum

---
+++ Target-sum.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+    num = list(input())
+    target = int(input())
+
+
+        print()
