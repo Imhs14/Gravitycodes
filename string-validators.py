ab490cd86a8c432fabfd3ebe120a7042a523c845
imhs14 <heerashanker0214@gmail.com>
2026-03-03 17:55:42 +0530

    string

--- string-validators.py
+++ string-validators.py
@@ -1,6 +1,13 @@
 if __name__ == '__main__':
     s = input()
-    
+    i = 0
+    while len(s[i])<1000 and len(s[i])>0:
+        print(any(char.isalnum() for char in s))
+        print(any(char.isalpha() for char in s))
+        print(any(char.isdigit() for char in s))
+        print(any(char.islower() for char in s))
+        print(any(char.isupper() for char in s))
+        break
 """
 example i/p , o/p :
 qA2
@@ -10,11 +17,9 @@ True
 True
 True
 """         
-
-
 """
-string.isalnum() - alpha numeric (a-z), (A-Z), (0-9)
-str.isalpha() - is alphabet (a-z), (A-Z)
+string.isalnum() - (a-z), (A-Z), (0-9)
+str.isalpha() - (a-z), (A-Z)
 str.isdigit() - (0-9)
 str.islower() - (a-z)
 str.isupper() - (A-Z)
