c4d8a33814c85e405d7428e888973a9faee927fc
imhs14 <heerashanker0214@gmail.com>
2026-03-03 17:55:31 +0530

    string validator done

---
+++ string-validators.py
@@ -0,0 +1,21 @@
+if __name__ == '__main__':
+    s = input()
+    
+"""
+example i/p , o/p :
+qA2
+True
+True
+True
+True
+True
+"""         
+
+
+"""
+string.isalnum() - alpha numeric (a-z), (A-Z), (0-9)
+str.isalpha() - is alphabet (a-z), (A-Z)
+str.isdigit() - (0-9)
+str.islower() - (a-z)
+str.isupper() - (A-Z)
+"""
